9fd2a8ad41834baa023f56cf5ec03ee47b13dd70
Jeff Greco <jeffgreco@gmail.com>
2026-01-11 18:45:14 -0600

    funny epople

---
+++ puzzle-builder/output/puzzle-2026-01-12T00-35-12-899Z.js
@@ -0,0 +1,30 @@
+// Add this to dailyPuzzles.js
+{
+  id: /* assign next available ID */,
+  theme: "Funny People Get Serious",
+  description: "",
+  movieIds: [
+    // EASY
+    8051, // Punch-Drunk Love (Oct 25, 2002)
+    10322, // Being There (Dec 19, 1979)
+    489, // Good Will Hunting (Jan 9, 1998)
+    38, // Eternal Sunshine of the Spotless Mind (Mar 19, 2004)
+    153, // Lost in Translation (Sep 18, 2003)
+
+    // MEDIUM
+    22318, // Permanent Midnight (Sep 16, 1998)
+    473033, // Uncut Gems (Dec 25, 2019)
+    873, // The Color Purple (Jan 16, 1987)
+    1125, // Dreamgirls (Dec 25, 2006)
+
+    // HARD
+    87492, // Foxcatcher (Jan 16, 2015)
+    17450, // Pennies from Heaven (Jan 1, 1982)
+    25793, // Precious (Nov 20, 2009)
+    86555, // Take This Waltz (May 25, 2012)
+    64690, // Drive (Sep 16, 2011)
+    1262, // Stranger Than Fiction (Nov 10, 2006)
+    403, // Driving Miss Daisy (Jan 26, 1990)
+    20829, // Funny People (Jul 31, 2009)
+  ],
+},
